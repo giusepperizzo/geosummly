f81dd7474190a78e3031e9e4b6b0ee186ef36196
jamfal <gia.falcone@gmail.com>
2014-01-18 18:43:33 +0100

    update R function

--- R file/plotFunction.R
+++ R file/plotFunction.R
@@ -1,7 +1,22 @@
-plot_data <-
-function(name) {
+#Parameters
+#input: input dataset (use complete path if you are on a different directory)
+#output: output density graph (specify a path if you want to save it on a different directory)
+
+#Steps
+#1. load ggplot library
+#2. read dataset
+#3. create a frame
+#4. delete latitude and longitude columns if they exist
+#5. rename columns (for graph legend)
+#6. stack up the values
+#7. rename the stack column corresponding to legend
+#8. plot the values (and scale them in order to have a better visualization)
+#9. save the graph
+
+plot_data <- function(input, output) {
   (library(ggplot2))
-  df <- as.data.frame(name)
+  src <- read.csv(input)
+  df <- as.data.frame(src)
   if(("Latitude" %in% names(df)) && ("Longitude" %in% names(df))) {
     df <- subset(df, select = -c(Latitude, Longitude))
   }
@@ -19,5 +34,5 @@ function(name) {
   names(dfs)[2]<-"legend"
   p <- ggplot(dfs, aes(x=values)) + geom_density(aes(group=legend, colour=legend, fill=legend), alpha=0.3)
   p + xlab("cells") + scale_x_log10()
-
+  ggsave(file=output, dpi=72)
 }
