069deca1b6c582597791848d7da6e07b3a31efc5
jamfal <gia.falcone@gmail.com>
2014-03-07 21:33:13 +0100

    refactoring for GeoJSONDecoder

--- src/main/java/it/unito/geosummly/GeoJSONDecoder.java
+++ src/main/java/it/unito/geosummly/GeoJSONDecoder.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import org.apache.commons.io.IOUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONWriter;
 import org.mapfish.geo.MfFeature;
 import org.mapfish.geo.MfFeatureCollection;
 import org.mapfish.geo.MfGeo;
@@ -67,5 +68,28 @@ public class GeoJSONDecoder {
 		}
     	return data;
     }
+}
+
+class FeatureTemplate extends MfFeature {
+	private String id;
+	private MfGeometry geometry;
+
+	public FeatureTemplate(String id, MfGeometry geometry, JSONObject properties) {
+		this.id=id;
+		this.geometry=geometry;
+	}
+	
+	@Override
+	public String getFeatureId() {
+		return id;
+	}
+
+	@Override
+	public MfGeometry getMfGeometry() {
+		return geometry;
+	}
+
+	@Override
+	public void toJSON(JSONWriter builder) throws JSONException {}
 
 }
