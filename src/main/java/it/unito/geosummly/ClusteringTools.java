c6a54687fd4711e7ae188f1946d56ddce67f15fa
jamfal <gia.falcone@gmail.com>
2014-03-18 17:16:34 +0100

    add ClusteringTools class

---
+++ src/main/java/it/unito/geosummly/ClusteringTools.java
@@ -0,0 +1,308 @@
+package it.unito.geosummly;
+
+import it.unito.geosummly.clustering.subspace.GEOSUBCLU;
+import it.unito.geosummly.clustering.subspace.InMemoryDatabase;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.TreeSet;
+import java.util.Vector;
+
+import org.apache.commons.csv.CSVRecord;
+
+import de.lmu.ifi.dbs.elki.algorithm.clustering.subspace.SUBCLU;
+import de.lmu.ifi.dbs.elki.data.Cluster;
+import de.lmu.ifi.dbs.elki.data.Clustering;
+import de.lmu.ifi.dbs.elki.data.DoubleVector;
+import de.lmu.ifi.dbs.elki.data.model.SubspaceModel;
+import de.lmu.ifi.dbs.elki.database.Database;
+import de.lmu.ifi.dbs.elki.database.ids.DBIDIter;
+import de.lmu.ifi.dbs.elki.database.ids.DBIDUtil;
+import de.lmu.ifi.dbs.elki.datasource.ArrayAdapterDatabaseConnection;
+import de.lmu.ifi.dbs.elki.datasource.filter.FixedDBIDsFilter;
+import de.lmu.ifi.dbs.elki.utilities.ClassGenericsUtil;
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.parameterization.ListParameterization;
+
+public class ClusteringTools {
+	
+	/**Fill in the matrix of normalized values from a list of CSV records.
+	 * The header won't be considered.
+	 * The column of timestamp values won't be considered.
+	*/
+	public ArrayList<ArrayList<Double>> buildNormalizedFromCSV(List<CSVRecord> list) {
+		ArrayList<ArrayList<Double>> matrix=new ArrayList<ArrayList<Double>>();
+		for(CSVRecord r: list) {
+			//we exclude the header
+			if(!r.get(0).contains("Timestamp")) {
+				ArrayList<Double> record=new ArrayList<Double>();
+				//we don't have to consider timepstamp values, so i=1
+				for(int i=1;i<r.size();i++)
+					record.add(Double.parseDouble(r.get(i)));
+				matrix.add(record);
+			}
+		}
+		return matrix;
+	}
+	
+	/**Fill in the matrix of normalized values from a list of list of double values.
+	 * The column of timestamp values won't be considered.
+	*/
+	public ArrayList<ArrayList<Double>> buildNormalizedFromList(ArrayList<ArrayList<Double>> inputMatrix) {
+		ArrayList<ArrayList<Double>> matrix=new ArrayList<ArrayList<Double>>();
+		ArrayList<Double> record;
+		for(ArrayList<Double> array: inputMatrix) {
+			record=new ArrayList<Double>();
+			//we don't have to consider timestamps, so j=1
+			for(int j=1;j<array.size();j++) {
+				record.add(array.get(j));
+			}
+			matrix.add(record);
+		}
+		return matrix;
+	}
+	
+	/**Fill in the feature hashmap from a list of CSV records.
+	 * Only single features will be considered, so feature combinations (i.e. with 'AND') will be excluded.
+	 * A feature will be added only if the corresponding value is greater than 0.
+	*/
+	public HashMap<Integer, String> getFeaturesMapFromDeltad(List<CSVRecord> list) {
+		HashMap<Integer, String> features=new HashMap<Integer, String>();
+		for(CSVRecord r: list) {
+			String f=(String) r.get(0).replace("deltad", "").replaceAll("\\(", "").replaceAll("\\)", ""); //take only feature name
+			if(!f.contains("AND") && Math.floor(Double.parseDouble(r.get(1)))>0) {
+				int mSize=features.size();
+				features.put(mSize+2, f); //keys start from 2
+			}
+		}
+		return features;
+	}
+	
+	/**Fill in the deltad hashmap from a list of CSV records.
+	 * Will be considered only that values which are greater than 0 and whose feature is in the features hashmap.*/
+	public  HashMap<String, Double> getValuesMapFromDeltad(List<CSVRecord> list) {
+		HashMap<String, Double> map=new HashMap<String, Double>();
+		ArrayList<String> toExclude=new ArrayList<String>(); //list of excluded features
+	    boolean excluded=false;
+	    boolean isFound=false;
+		for(CSVRecord r: list) {
+			String feature=(String) r.get(0).replace("deltad", "").replaceAll("\\(", "").replaceAll("\\)", ""); //take only feature name
+			excluded=false;
+			isFound=false;
+			for(int i=0;i<toExclude.size() && !isFound;i++) {
+				if(feature.contains(toExclude.get(i))) {
+					toExclude.add(feature);
+					excluded=true;
+					isFound=true;
+				}
+			}
+			double d=Math.floor(Double.parseDouble(r.get(1))); //floor of deltad value
+			if(d > 0 && !excluded) {
+				map.put(feature, d);
+			}
+			else if(!excluded)
+				toExclude.add(feature);
+		}
+		return map;
+	}
+	
+	/**
+	 * Fill in the cells hashmap with a the cells of a cluster.
+	 * Each entry of the map will be a couple: key, list_of_triple (cell_id, lat, lng).
+	*/
+	public HashMap<Integer, ArrayList<ArrayList<Double>>> putCompleteCellsOfCluster(HashMap<Integer, ArrayList<ArrayList<Double>>> cellsOfCluster, Cluster<?> cluster, int index, List<CSVRecord> listDens) {
+		int cellId=0;
+		String cellLat="";
+		String cellLng="";
+		ArrayList<Double> cellRecord;
+		ArrayList<ArrayList<Double>> cells=new ArrayList<ArrayList<Double>>();
+		
+		for(DBIDIter iter=cluster.getIDs().iter(); iter.valid(); iter.advance()) {
+			cellId=Integer.parseInt(DBIDUtil.toString(iter));
+			cellLat=listDens.get(cellId).get(1); //latitude
+			cellLng=listDens.get(cellId).get(2); //longitude
+			cellRecord=new ArrayList<Double>();
+			cellRecord.add((double) cellId);
+			cellRecord.add(Double.parseDouble(cellLat));
+			cellRecord.add(Double.parseDouble(cellLng));
+			cells.add(cellRecord); //add a triple: id, lat, lng
+		}
+		cellsOfCluster.put(index, cells);
+		
+		return cellsOfCluster;
+	}
+	
+	/**
+	 * Fill in the cells hashmap with the cell_ids of a cluster.
+	 * Each entry of the map will be a couple: key, list of cell_ids.
+	*/
+	public HashMap<Integer, ArrayList<Integer>> putIdCellsOfCluster(HashMap<Integer, ArrayList<Integer>> cellsOfCluster, Cluster<?> cluster, int index) {
+		int cellId=0;
+		ArrayList<Integer> cells=new ArrayList<Integer>();
+		for(DBIDIter iter=cluster.getIDs().iter(); iter.valid(); iter.advance()) {
+			cellId=Integer.parseInt(DBIDUtil.toString(iter));
+			cells.add(cellId); //add a triple: id, lat, lng
+		}
+		cellsOfCluster.put(index, cells);
+		return cellsOfCluster;
+	}
+	
+	/**
+	 * Fill in the venues hashmap with the venues of the cells of a cluster.
+	 * Each entry of the map will be a couple: key=cellId, list of lists of venue_info.
+	*/
+	public  HashMap<Integer, ArrayList<ArrayList<String>>> putVenuesOfCells(HashMap<Integer, ArrayList<ArrayList<String>>> venuesOfCell, ArrayList<ArrayList<Double>> cells, List<CSVRecord> listSingles, HashMap<Integer, String> featuresMap) {
+		int cellId=0;
+		String cellLat="";
+		String cellLng="";
+		ArrayList<ArrayList<String>> venuesInfo;
+		boolean found=false;
+		boolean added=false;
+		ArrayList<String> venueRecord;
+		boolean catFound=false;
+		for(ArrayList<Double> array: cells) {
+			cellId=array.get(0).intValue();
+			cellLat=array.get(1)+"";
+			cellLng=array.get(2)+"";
+			venuesInfo=new ArrayList<ArrayList<String>>();
+			found=false;
+			added=false;
+			for(int i=0;i<listSingles.size() && !found;i++) {
+				CSVRecord r=listSingles.get(i); //venue information
+				//we don't have to consider the header
+				if(!r.get(0).contains("Timestamp")) {
+					//check if the venue belong to the cell
+					if(r.get(5).equals(cellLat) && r.get(6).equals(cellLng)) {
+						venueRecord=new ArrayList<String>();
+						venueRecord.add(r.get(0)); //timestamp
+						venueRecord.add(r.get(1)); //beenHere
+						venueRecord.add(r.get(2)); //venue id
+						venueRecord.add(r.get(3)); //venue lat
+						venueRecord.add(r.get(4)); //venue lat
+						venueRecord.add(r.get(5)); //focal lat
+						venueRecord.add(r.get(6)); //focal lng
+						catFound=false;
+						//venue category
+						for(int h=7;h<r.size() && !catFound;h++)
+							if(r.get(h).equals("1.0")) {
+								venueRecord.add(featuresMap.get(h-7+2));
+								catFound=true;
+							}
+						venuesInfo.add(venueRecord); //add the venue informations
+						added=true;
+					} else if(added) 
+						found=true; //since venues of the same cell are consecutive, we stop the loop once we found different focal coordinate values
+				}
+			}
+			//add venue_id only if the venue exists in the cell
+			if(added)
+				venuesOfCell.put(cellId, venuesInfo);
+		}
+		
+		return venuesOfCell;
+	}
+	
+	/**
+	 * Get all the distinct cluster labels.
+	 */
+	public TreeSet<String> getClusterLabels(HashMap<Integer, String> clusters) {
+		ArrayList<Integer> keys=new ArrayList<Integer>(clusters.keySet());
+	    TreeSet<String> tree =new TreeSet<String>();
+	    for(Integer i: keys) {
+	    	tree.add(clusters.get(i));
+	    }
+	    return tree;
+	}
+	
+	/**
+	 * Get all the cell corresponding to the cluster label.
+	 */
+	public ArrayList<TreeSet<Integer>> getCellsOfClusters(HashMap<Integer, String> clusters, HashMap<Integer, ArrayList<Integer>> cells, TreeSet<String> tree) {
+	    Iterator<String> iter=tree.iterator();
+	    ArrayList<Integer> keys=new ArrayList<Integer>(clusters.keySet());
+	    ArrayList<TreeSet<Integer>> allCells =new ArrayList<TreeSet<Integer>>();
+	    TreeSet<Integer> cellIndex;
+	    String label="";
+	    while(iter.hasNext()) {
+	    	label=iter.next();
+	    	cellIndex=new TreeSet<Integer>();
+	    	for(Integer i: keys) {
+	    		if(clusters.get(i).equals(label)) {
+	    			cellIndex.addAll(cells.get(i));
+	    		}
+	    	}
+	    	allCells.add(cellIndex);
+	    }
+	    return allCells;
+	}
+	
+	/**
+	 * Create an hashmap for the holdout used to compute Jaccard evaluation.
+	 * each entry of the map will be a couple: cluster_name, list_of_cells 
+	*/
+	public  HashMap<String, Vector<Integer>> buildHoldoutMap(TreeSet<String> distinctLabels, ArrayList<TreeSet<Integer>> allCells, int length) {
+		HashMap<String, Vector<Integer>> holdout=new HashMap<String, Vector<Integer>>();
+	    Vector<Integer> vector;
+	    Iterator<String> distinctIter=distinctLabels.iterator();
+        Iterator<TreeSet<Integer>> allCellsIter=allCells.iterator();
+        while(distinctIter.hasNext() && allCellsIter.hasNext()) {
+        	String label=distinctIter.next();
+        	Iterator<Integer> treeIter=allCellsIter.next().iterator();
+        	vector=new Vector<Integer>();
+        	while(treeIter.hasNext()) {
+        		vector.add(treeIter.next()-length);
+        	}
+        	holdout.put(label, vector);
+        }
+        
+        return holdout;
+	}
+	
+	/**Set SUBCLU parameters and run the algorithm*/
+    public Clustering<?> runSUBCLU (Database db, double eps, int minpts) {
+        ListParameterization params = new ListParameterization();
+        params.addParameter(SUBCLU.EPSILON_ID, eps);
+        params.addParameter(SUBCLU.MINPTS_ID, minpts);
+        
+        // setup algorithm
+        SUBCLU<DoubleVector> subclu = ClassGenericsUtil.parameterizeOrAbort(SUBCLU.class, params);
+
+        // run SUBCLU on database
+        Clustering<SubspaceModel<DoubleVector>> result = subclu.run(db);
+        return result;
+    }
+	
+	/**Set GEOSUBCLU parameters and run the algorithm*/
+    public Clustering<?> runGEOSUBCLU (Database db, HashMap<Integer, String> map, HashMap<String, Double>deltad, int density, double eps) {
+        ListParameterization params = new ListParameterization();
+        
+        // setup algorithm
+        GEOSUBCLU<DoubleVector> geosubclu = ClassGenericsUtil.parameterizeOrAbort(GEOSUBCLU.class, params);
+        geosubclu.setFeatureMapper(map);
+        geosubclu.setDeltad(deltad);
+        geosubclu.setDensity(density);
+        geosubclu.setEpsValue(eps);
+
+        // run GEOSUBCLU on database
+        Clustering<SubspaceModel<DoubleVector>> result = geosubclu.run(db);
+        return result;
+    }
+	
+	/**Build a Database from the matrix of normalized density values*/
+    public <T> Database buildDatabaseFromMatrix (ArrayList<ArrayList<Double>> matrix) {       
+        double[][] data = new double[matrix.size()][];
+        for (int i=0; i<matrix.size(); i++) {
+            data[i] = new double[matrix.get(i).size()];
+            for(int j=0; j<matrix.get(i).size(); j++) {
+                data[i][j] = (matrix.get(i)).get(j);
+            }
+        }
+        
+        List<Class<?>> filterlist = new ArrayList<>();
+        filterlist.add(FixedDBIDsFilter.class);
+        Database db = new InMemoryDatabase(new ArrayAdapterDatabaseConnection(data), null);        
+        db.initialize();
+        return db;
+    }
+}
