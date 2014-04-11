09ebecde9c852b32f58f259c3645dc1e72b060e7
jamfal <gia.falcone@gmail.com>
2014-04-11 17:03:50 +0200

    add method on GeoJSONReader.java for reading geojson file for optimization state

--- src/main/java/it/unito/geosummly/io/GeoJSONReader.java
+++ src/main/java/it/unito/geosummly/io/GeoJSONReader.java
@@ -1,9 +1,15 @@
 package it.unito.geosummly.io;
 
 import it.unito.geosummly.BoundingBox;
+import it.unito.geosummly.io.templates.FeatureCollectionTemplate;
+import it.unito.geosummly.io.templates.FeatureTemplate;
+import it.unito.geosummly.io.templates.SamplingFeatureTemplate;
 
+
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
@@ -11,7 +17,6 @@ import java.util.ArrayList;
 import org.apache.commons.io.IOUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.json.JSONWriter;
 import org.mapfish.geo.MfFeature;
 import org.mapfish.geo.MfFeatureCollection;
 import org.mapfish.geo.MfGeo;
@@ -19,34 +24,35 @@ import org.mapfish.geo.MfGeoFactory;
 import org.mapfish.geo.MfGeoJSONReader;
 import org.mapfish.geo.MfGeometry;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.Polygon;
 
 public class GeoJSONReader {
-	
-	private MfGeoFactory mfFactory;
-    private MfGeoJSONReader reader;
     
-    public GeoJSONReader() {
-    	mfFactory = new MfGeoFactory() {
+	/**
+	 * Decode a geojson file for the sampling state
+	 */
+    public ArrayList<BoundingBox> decodeForSampling(String file) throws IOException, JSONException {
+    	
+    	MfGeoFactory mfFactory = new MfGeoFactory() {
     		public MfFeature createFeature(String id, MfGeometry geometry, JSONObject properties) {
-    			return new InputFeatureTemplate(id, geometry, properties);
+    			return new SamplingFeatureTemplate(id, geometry, properties);
             }
         };
-        reader = new MfGeoJSONReader(mfFactory);
-    }
-    
-    public ArrayList<BoundingBox> decode(String path) throws IOException, JSONException {
+        MfGeoJSONReader reader = new MfGeoJSONReader(mfFactory);
+    	
     	ArrayList<BoundingBox> data=new ArrayList<BoundingBox>();
-    	File f=new File(path);
+    	File f=new File(file);
     	InputStream in= new FileInputStream(f);
 		String str= IOUtils.toString(in);
 		in.close();
 		MfGeo result = reader.decode(str); //decode geojson file given as a String
 		MfFeatureCollection collection= (MfFeatureCollection) result;
 		ArrayList<MfFeature> coll = (ArrayList<MfFeature>) collection.getCollection(); //all the geojson features
-		InputFeatureTemplate feature;
-		MfGeometry featureGeomtry; 
+		SamplingFeatureTemplate feature;
+		MfGeometry featureGeometry; 
 		Geometry jts;
 		Polygon polygon;
 		double north;
@@ -54,43 +60,52 @@ public class GeoJSONReader {
 		double south;
 		double west;;
 		BoundingBox b;
+		
 		for(MfFeature mf: coll) {
-			feature=(InputFeatureTemplate) mf;
-			featureGeomtry=feature.getMfGeometry(); //get the feature geometry
-			jts=featureGeomtry.getInternalGeometry();
+			
+			feature=(SamplingFeatureTemplate) mf;
+			featureGeometry=feature.getMfGeometry(); //get the feature geometry
+			jts=featureGeometry.getInternalGeometry();
+			
 			polygon=(Polygon) jts; //feature geometry is a polygon
 			north=polygon.getExteriorRing().getPointN(0).getCoordinate().y; //get the bbox coordinates
 			east=polygon.getExteriorRing().getPointN(1).getCoordinate().x;
 			south=polygon.getExteriorRing().getPointN(2).getCoordinate().y;
 			west=polygon.getExteriorRing().getPointN(3).getCoordinate().x;
+			
 			b=new BoundingBox(north, east, south, west);
 			data.add(b);
 		}
+		
     	return data;
     }
-}
-
-/**Input template of a feature */
-class InputFeatureTemplate extends MfFeature {
-	private String id;
-	private MfGeometry geometry;
-
-	public InputFeatureTemplate(String id, MfGeometry geometry, JSONObject properties) {
-		this.id=id;
-		this.geometry=geometry;
-	}
-	
-	@Override
-	public String getFeatureId() {
-		return id;
-	}
-
-	@Override
-	public MfGeometry getMfGeometry() {
-		return geometry;
-	}
-
-	@Override
-	public void toJSON(JSONWriter builder) throws JSONException {}
-
+    
+    /**
+	 * Decode a geojson file for the optimization state
+	 */
+    public ArrayList<ArrayList<Integer>> decodeForOptimization(String file) throws IOException {
+    	
+    	BufferedReader br = new BufferedReader(
+				new FileReader(file));
+		Gson gson=new GsonBuilder().setPrettyPrinting().create();
+		FeatureCollectionTemplate fct=gson.fromJson(br, FeatureCollectionTemplate.class);
+		ArrayList<FeatureTemplate> ft_array=fct.getFeatures();
+		
+		ArrayList<Integer> objOfClusters=new ArrayList<Integer>();
+		ArrayList<Integer> venueOfClusters=new ArrayList<Integer>();
+		ArrayList<Integer> labelOfClusters=new ArrayList<Integer>();
+		
+		for(FeatureTemplate ft: ft_array) {
+			objOfClusters.add(ft.getGeometry().getCoordinates().size());
+			venueOfClusters.add(ft.getProperties().getVenues().size());
+			labelOfClusters.add(ft.getProperties().getName().trim().split(",").length);
+		}
+		
+		ArrayList<ArrayList<Integer>> infos=new ArrayList<ArrayList<Integer>>();
+		infos.add(objOfClusters);
+		infos.add(venueOfClusters);
+		infos.add(labelOfClusters);
+		
+		return infos;
+    }
 }
