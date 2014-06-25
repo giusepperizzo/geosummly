9cb696ca1afbedbabfac5a2a10e1ebc3e8f547c5
Giuseppe Rizzo <giuse.rizzo@gmail.com>
2014-06-25 10:36:47 +0200

    [ui] update config.js to list the pareto outputs

--- src/main/resources/webroot/app/js/config/config.js
+++ src/main/resources/webroot/app/js/config/config.js
@@ -33,8 +33,28 @@ app.Config = function() {
 				},
 				zoom: 11
 			}),		
+			athens_pareto: new Location({
+				jsonUrl: 'data/athens/pareto-clustering-output-eps0.039283710065919304.geojson',
+				bounds: {
+					north: 38.076203576652,
+					sud: 37.9323527,
+					west: 23.628845214844,
+					est: 23.8113263
+				},
+				zoom: 11
+			}),					
 			athens_eps005: new Location({
 				jsonUrl: 'data/athens/clustering-output-eps0.05.geojson',
+				bounds: {
+					north: 38.076203576652,
+					sud: 37.9323527,
+					west: 23.628845214844,
+					est: 23.8113263
+				},
+				zoom: 11
+			}),	
+			athens_eps005_pareto: new Location({
+				jsonUrl: 'data/athens/pareto-clustering-output-eps0.05.geojson',
 				bounds: {
 					north: 38.076203576652,
 					sud: 37.9323527,
@@ -52,7 +72,17 @@ app.Config = function() {
 					est: 25.192337036133
 				},
 				zoom: 13
-			}),		
+			}),
+			heraklion_pareto: new Location({
+				jsonUrl: 'data/heraklion/pareto-clustering-output-eps0.08838834764831845.geojson',
+				bounds: {
+					north: 35.344815237811,
+					sud:  35.2773249,
+					west: 25.109596252441,
+					est: 25.192337036133
+				},
+				zoom: 13
+			}),						
 			heraklion_eps01: new Location({
 				jsonUrl: 'data/heraklion/clustering-output-eps0.1.geojson',
 				bounds: {
@@ -62,7 +92,17 @@ app.Config = function() {
 					est: 25.192337036133
 				},
 				zoom: 13
-			}),						
+			}),
+			heraklion_eps01_pareto: new Location({
+				jsonUrl: 'data/heraklion/pareto-clustering-output-eps0.1.geojson',
+				bounds: {
+					north: 35.344815237811,
+					sud:  35.2773249,
+					west: 25.109596252441,
+					est: 25.192337036133
+				},
+				zoom: 13
+			}),									
 			london: new Location({
 				jsonUrl: 'data/london/clustering-output-eps0.02525381361380527.geojson',
 				bounds: {
@@ -73,6 +113,16 @@ app.Config = function() {
 				},
 				zoom: 11
 			}),
+			london_pareto: new Location({
+				jsonUrl: 'data/london/pareto-clustering-output-eps0.02525381361380527.geojson',
+				bounds: {
+					north: 51.58389660297623,
+					sud:  51.3591295,
+					west: -0.35980224609375,
+					est: 0.0019242
+				},
+				zoom: 11
+			}),			
 			london_eps004: new Location({
 				jsonUrl: 'data/london/clustering-output-eps0.04.geojson',
 				bounds: {
@@ -82,7 +132,17 @@ app.Config = function() {
 					est: 0.0019242
 				},
 				zoom: 11
-			}),		
+			}),
+			london_eps004_pareto: new Location({
+				jsonUrl: 'data/london/pareto-clustering-output-eps0.04.geojson',
+				bounds: {
+					north: 51.58389660297623,
+					sud:  51.3591295,
+					west: -0.35980224609375,
+					est: 0.0019242
+				},
+				zoom: 11
+			}),					
 			london_eps005: new Location({
 				jsonUrl: 'data/london/clustering-output-eps0.05.geojson',
 				bounds: {
@@ -92,9 +152,9 @@ app.Config = function() {
 					est: 0.0019242
 				},
 				zoom: 11
-			}),	
-			london_eps006: new Location({
-				jsonUrl: 'data/london/clustering-output-eps0.06.geojson',
+			}),
+			london_eps005_pareto: new Location({
+				jsonUrl: 'data/london/pareto-clustering-output-eps0.05.geojson',
 				bounds: {
 					north: 51.58389660297623,
 					sud:  51.3591295,
@@ -102,7 +162,7 @@ app.Config = function() {
 					est: 0.0019242
 				},
 				zoom: 11
-			}),
+			}),	
 			milan: new Location({
 				jsonUrl: 'data/milan/clustering-output-eps0.014142135623730952.geojson',
 				bounds: {
@@ -132,7 +192,17 @@ app.Config = function() {
 					est: 9.312688264185255
 				},
 				zoom: 11
-			}),						
+			}),
+			milan_eps002_pareto: new Location({
+				jsonUrl: 'data/milan/pareto-clustering-output-eps0.02.geojson',
+				bounds: {
+					north: 45.567794914783256,
+					sud:  45.35668565341512,
+					west: 9.011490619692509,
+					est: 9.312688264185255
+				},
+				zoom: 11
+			}),										
 			milan_eps003: new Location({
 				jsonUrl: 'data/milan/clustering-output-eps0.03.geojson',
 				bounds: {
@@ -142,7 +212,17 @@ app.Config = function() {
 					est: 9.312688264185255
 				},
 				zoom: 11
-			}),		
+			}),
+			milan_eps003_pareto: new Location({
+				jsonUrl: 'data/milan/pareto-clustering-output-eps0.03.geojson',
+				bounds: {
+					north: 45.567794914783256,
+					sud:  45.35668565341512,
+					west: 9.011490619692509,
+					est: 9.312688264185255
+				},
+				zoom: 11
+			}),
 			/*trentino: new Location({
 				jsonUrl: 'data/trentino/clustering-output-eps0.09.geojson',
 				bounds: {
@@ -162,7 +242,17 @@ app.Config = function() {
 					est: 7.734889984130859
 				},
 				zoom: 12
-			})
+			}),
+			turin_pareto: new Location({
+				jsonUrl: 'data/turin/clustering-output-eps0.07071067811865477.geojson',
+				bounds: {
+					north: 45.10975600522702,
+					sud: 45.04393354716772,
+					west: 7.630176544189453,
+					est: 7.734889984130859
+				},
+				zoom: 12
+			})			
 		}
 	};
 };
