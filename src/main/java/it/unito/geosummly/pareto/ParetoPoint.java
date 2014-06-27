efa395bea551aec0b10da81a14bb45d10a0ea8db
jamfal <gia.falcone@gmail.com>
2014-06-27 14:57:48 +0200

    ParetoPoint class: replace comma with points on double values (due to the exceptions only on windows platform)

--- src/main/java/it/unito/geosummly/pareto/ParetoPoint.java
+++ src/main/java/it/unito/geosummly/pareto/ParetoPoint.java
@@ -18,36 +18,36 @@ public class ParetoPoint {
 	}
 	public ParetoPoint(int i, Double x, Double y, Double z, Double s) {
 		this.setIndex(i);
-		this.x = Double.valueOf( df.format(x) );
-		this.y = Double.valueOf( df.format(y) );
-		this.z = Double.valueOf( df.format(z) );
-		this.s = Double.valueOf( df.format(s) );
+		this.x = Double.valueOf( df.format(x).replace(",", ".") );
+		this.y = Double.valueOf( df.format(y).replace(",", ".") );
+		this.z = Double.valueOf( df.format(z).replace(",", ".") );
+		this.s = Double.valueOf( df.format(s).replace(",", ".") );
 	}
 	public Double getX() {
 		return x;
 	}
 	public void setX(Double x) {
-		this.x = Double.valueOf( df.format(x) );
+		this.x = Double.valueOf( df.format(x).replace(",", ".") );
 	}
 	
 	public Double getY() {
 		return y;
 	}
 	public void setY(Double y) {
-		this.y = Double.valueOf( df.format(y) );
+		this.y = Double.valueOf( df.format(y).replace(",", ".") );
 	}
 
 	public Double getZ() {
 		return z;
 	}
 	public void setZ(Double z) {
-		this.z = Double.valueOf( df.format(z) );
+		this.z = Double.valueOf( df.format(z).replace(",", ".") );
 	}
 	
 	public Double getS() {
 		return s;
 	}
 	public void setS(Double s) {
-		this.s = Double.valueOf( df.format(s) );
+		this.s = Double.valueOf( df.format(s).replace(",", ".") );
 	}
 }
